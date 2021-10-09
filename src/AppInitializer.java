7b13030b8dc096cb8faac49c01012b235b03cc61
kandage <kandagehiruni@gmail.com>
2021-10-09 10:01:04 +0530

    added app initializer class

---
+++ src/AppInitializer.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  *
+ *  *  * ---------------------------------------------------------------------------------------------
+ *  *  *  *  Copyright (c)Scince 2021 - present IJSE. All rights reserved.
+ *  *  *  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  *  *  *--------------------------------------------------------------------------------------------
+ *  *
+ *
+ *
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Hiruni Kandage <kandagehiruni@gmail.com>
+ * @since 10/9/2021
+ */
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}

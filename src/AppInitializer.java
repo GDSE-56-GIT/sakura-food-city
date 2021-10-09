4a83bdf66ee2872f0973384b5c27f21162bb09f2
kandage <kandagehiruni@gmail.com>
2021-10-09 11:00:27 +0530

    added database login form with controller class

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -11,6 +11,9 @@
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -24,6 +27,11 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(this.getClass().getResource("view/LoginForm.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Login Form");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import db.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public AnchorPane root;
+    public static String userID;
+    public static String name;
+
+
+}
